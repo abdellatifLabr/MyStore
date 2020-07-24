05a6bd75e08bb2b86cedc931006fe4df8e9ce209
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-07-24 11:37:23 +0100

    SMTP settings

--- src/mystore/settings.py
+++ src/mystore/settings.py
@@ -116,3 +116,12 @@ STATIC_URL = '/static/'
 
 # Cors
 CORS_ORIGIN_ALLOW_ALL = True
+
+# SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
