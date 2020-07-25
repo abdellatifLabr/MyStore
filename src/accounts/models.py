4075e639d9ab56ba1a5186662cbd4c0b02b7a845
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-07-25 16:43:26 +0100

    Create profile automatically after user created

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.conf import settings
 
-# Create your models here.
+from profiles.models import Profile
+
+def create_profile(sender, instance, **kwargs):
+    Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
