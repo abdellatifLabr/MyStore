07799581028a27f782a3ffbd0ca0c4c5faccd5a0
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-07-27 18:27:51 +0100

    Create a cart for every user after sign up

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,8 +3,10 @@ from django.db.models.signals import post_save
 from django.conf import settings
 
 from profiles.models import Profile
+from shopping.models import Cart
 
 def create_profile(sender, instance, **kwargs):
     Profile.objects.create(user=instance)
+    Cart.objects.create(user=instance)
 
 post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
