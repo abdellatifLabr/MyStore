a05f861f5f8aed51543cfe32930b48e29314ca82
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-07-29 14:56:25 +0100

    Created core app to manage utils functions and constants

---
+++ src/core/constants.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext as _
+
+class Messages:
+    NO_PERMISSION = {
+        'message': _('You don\'t have the permission to perform this action'),
+        'code': 'no_permission'
+    }
+    
--- src/mystore/settings.py
+++ src/mystore/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'corsheaders',
     'graphene_django',
     'graphql_auth',
