3ab5a3b8ff8fae4e6674b8ce316f94111fef706b
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-03 19:27:39 +0100

    Prevent creating a profile if the user is staff

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -6,7 +6,8 @@ from profiles.models import Profile
 from shopping.models import Cart
 
 def create_profile(sender, instance, **kwargs):
-    Profile.objects.create(user=instance)
-    Cart.objects.create(user=instance)
+    if not instance.is_staff:
+        Profile.objects.create(user=instance)
+        Cart.objects.create(user=instance)
 
 post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
