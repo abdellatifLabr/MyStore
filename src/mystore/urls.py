3856bc41743a784462cbcd2837573cec0541cdaf
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-03 14:30:57 +0100

    File upload impelemntation

--- src/mystore/urls.py
+++ src/mystore/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=False))),
+    path('graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=False))),
 ]
