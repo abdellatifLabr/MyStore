c817f564232b889de75585f766e3ea8795df0ea9
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-07 16:25:35 +0100

    Added django sites

--- src/mystore/settings.py
+++ src/mystore/settings.py
@@ -17,6 +17,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -27,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'core',
     'corsheaders',
     'graphene_django',
