402441a210738f7cc63e59e0f3e2216b56307290
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-09-08 14:34:39 +0100

    Create default avatar on profile creation

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
 
-from profiles.models import Profile
+from profiles.models import Profile, Avatar
 
 def create_profile(sender, instance, created, **kwargs):
     if not instance.is_staff:
         if created:
-            Profile.objects.create(user=instance)
+            avatar = Avatar()
+            avatar.save()
+            Profile.objects.create(user=instance, avatar=avatar)
 
 post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from phone_field import PhoneField
 from imagekit.models import ProcessedImageField, ImageSpecField
 from imagekit.processors import ResizeToFill
+from django.db.models.signals import post_save
 
 from .utils import build_avatar_path
 
@@ -40,7 +41,7 @@ class Avatar(models.Model):
 class Profile(models.Model):
     bio = models.CharField(max_length=255, blank=True)
     phone = PhoneField(null=True)
-    avatar = models.OneToOneField(Avatar, on_delete=models.CASCADE, null=True)
+    avatar = models.OneToOneField(Avatar, on_delete=models.CASCADE)
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -48,3 +49,8 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
         
+def create_avatar(sender, instance, created, **kwargs):
+    if created:
+        instance.avatar = Avatar()
+
+post_save.connect(create_avatar, sender=Profile)
