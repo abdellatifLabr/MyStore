c02823f2177321ab5cf0d121e0d19257c3cd9af7
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-09-19 14:34:48 +0100

    asgi cors

--- mystore/routing.py
+++ mystore/routing.py
@@ -1,17 +1,16 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import OriginValidator
+from channels.security.websocket import OriginValidator, AllowedHostsOriginValidator
 from django.conf import settings
 
 import notifications.routing
 
 application = ProtocolTypeRouter({
-    'websocket': OriginValidator(
+    'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
                 notifications.routing.websocket_urlpatterns
             )
-        ),
-        settings.CORS_ALLOWED_ORIGINS or '*'
+        )
     ),
 })
